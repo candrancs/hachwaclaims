eba27b410325053d1608f65eea0c178da1da48cf
Who Am I <41993012+candrancs@users.noreply.github.com>
2020-02-17 22:53:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "xxxxxxxx";
+$address = "xxxxxxxxx";
+
+/* Link Pass :    https://semawur.com/0dGrBL7wm0   */
+
+$pass_scrip = "xxxxx";
+
+
+//https://www.hachwaclaims.xyz/?r=13828
+
+
